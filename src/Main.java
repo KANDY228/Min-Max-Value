e9da701be11cf291ca669feac0a4bc8d91096361
KANDY228 <71720867+KANDY228@users.noreply.github.com>
2022-09-24 19:12:18 +0300

    Min Max Value

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// float
+		System.out.println("Min-Max Value ( Float )");
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		
+		// double
+		System.out.println("Min-Max Value ( Double )");
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+	}
+}
